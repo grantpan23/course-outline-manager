c334b8158cc2dd5d6f397a579ab33c3a308f173a
grantpanman <gpan7@uwo.ca>
2023-01-26 20:27:52 -0500

    Backend setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+//express and port
+const express = require('express');
+const app = express();
+const port = 4000;
+
+//import routes
+const admin = require('./routes/admin');
+
+//middleware
+app.use('/api/admin', admin);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
