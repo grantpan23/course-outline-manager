26d6a7b320446381b26533b3d1a979b2885a693a
grantpanman <gpan7@uwo.ca>
2023-02-01 16:25:49 -0500

    Secured DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 //express and port
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 //import routes
 const admin = require('./routes/admin');
@@ -9,9 +11,18 @@ const admin = require('./routes/admin');
 //middleware
 app.use('/api/admin', admin);
 
+//DB Connection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log('DB connected.')
+})
+.catch((err) => {
+    console.log(err);
+})
+
 app.get('/', (req,res) => {
     res.send('works');
-})
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
