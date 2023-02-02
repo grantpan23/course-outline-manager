6b2746531189188f57075101d317adefb08fc26e
ademidunt <71097538+ademidunt@users.noreply.github.com>
2023-02-02 15:21:42 -0500

    created file for createOutline

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Login from './login';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import CreateOutline from './createOutline';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <Login />
+    <CreateOutline />
   </React.StrictMode>
 );
 
