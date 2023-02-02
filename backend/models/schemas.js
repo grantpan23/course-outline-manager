3fa4fd470ea575fc84684ea89f4675dc090155c9
grantpanman <gpan7@uwo.ca>
2023-02-01 19:13:59 -0500

    Revise schema

--- backend/models/schemas.js
+++ backend/models/schemas.js
@@ -1,18 +1,52 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const instructorSchema = new Schema({
-    email: {type:String, required:true},
-    firstName: {type:String, required:true},
-    lastName: {type:String, required:true}
-});
+//schema creations
+const userSchema = new Schema({
+    email: {type:String, unique:true, required:true},
+    password: {type:String, default:'changeme', required:true},
+    username: {type:String, required:true, unique:true},
+    role: {
+        type:String, 
+        enum: ['instructor', 'reviewer', 'admin'],
+        required:true
+    },
+    firstName: {type:String,required:true},
+    lastName: {type:String,required:true}
+})
 
 const courseSchema = new Schema({
-
+    name:{type:String,required:true},
+    code:{type:String,required:true},
+    faculty:{type:String,required:true},
+    instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      }
 });
 
-const Instructors = mongoose.model('instructors', instructorSchema, 'instructors');
+//middleware
+userSchema.pre('save', function (next) {
+    if (this.role !== 'instructor') {
+        this.courses = undefined;
+      }
 
-const mySchemas = {'Instructors':Instructors};
+    this.username = this.email.split('@')[0];
+    next();
+  });
+
+  userSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'instructor'
+  });
+
+  //model creations
+const User = mongoose.model('User', userSchema);
+const Course = mongoose.model('Course',courseSchema);
+
+//aggregate models
+const mySchemas = {'User':User,'Course':Course};
 
 module.exports = mySchemas;
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,16 +4,31 @@ const Schemas = require('../models/schemas.js');
 
 router.use(express.json());
 
+//change to router.route, .get, etc
 router.get('/instructors', (req,res) => {
-    const instructors = Schemas.Instructors;
+    const users = Schemas.User;
 
-    instructors.find({}, (err,data) => {
+    users.find({role:'instructor'}, (err,data) => {
         if(err){
             res.status(500).send(err);
         } else {
-            res.json(data);
+            // const instructors = data.map(instructor => {
+            //     return {
+            //       firstName: instructor.firstName,
+            //       lastName: instructor.lastName,
+            //       username: instructor.username,
+            //       courses: instructor.courses
+            //     };
+            //   });
+            //   res.json(instructors); 
+            data.forEach(instructor => {
+                instructor.courses = instructor.getCourses();
+              });
+              res.json(data);
         }
     })
 })
+
+router.route('/edit-course/:instructorID',)
 
 module.exports = router;
