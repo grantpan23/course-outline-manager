257caec33b171829a98f55b0ac711c34250a8fe2
jtan334 <jtan334@uwo.ca>
2023-02-15 15:32:36 -0500

    added boilerplate to frontend

--- frontend/src/instructorHistory.js
+++ frontend/src/instructorHistory.js
@@ -1,14 +1,40 @@
-import React, {useRef, useState} from 'react'
-import {Link, useNavigate} from "react-router-dom";
+import React, {useRef, useState, ReactDOM} from 'react'
+import { useNavigate} from "react-router-dom";
 
 
 export default function InstructionHistory() {
 
+    const [error, setError] =useState("")
+    const [loading, setLoading] =useState(false)
+    const history =useNavigate()
 
-   
+
+  //  const editor = document.createElement('div')
+
+   //const root = ReactDOM.createRoot(
+  //document.getElementById('root'));
+
     return(
 
-
-        <h1>hi</h1>
+        <div>
+        <h1>Courses and Past Instructors</h1>
+        
+          <div>
+          <table>
+  <tr>
+    <th>Courses</th>
+    <th>Instructors</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+  </tr>
+</table>
+          </div>
+        </div>
     )
 }
