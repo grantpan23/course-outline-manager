d45ca1779ea08b256ecf8ee78dacfc3c71713c1f
pfornasi <pfornasi@uwo.ca>
2023-03-10 10:37:52 -0500

    not much

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,12 @@ require('dotenv/config');
 //import routes
 const admin = require('./routes/admin');
 const auth = require('./routes/auth');
-const edits = require('./routes/edits');
+// const edits = require('./routes/edits');
 
 //middleware
 app.use('/api/admin/:adminUser', admin);
 app.use('/api/auth', auth);
-app.use('/api/edits', edits);
+// app.use('/api/edits', edits);
 
 //DB Connection
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
