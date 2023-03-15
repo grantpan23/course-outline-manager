99eb478739d63781e1e22361379deb5da47f5e18
grantpanman <gpan7@uwo.ca>
2023-03-15 17:23:19 -0400

    Made styles folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styles.css"
+import "./styles/styles.css"
 import {BrowserRouter } from "react-router-dom";
 
 
