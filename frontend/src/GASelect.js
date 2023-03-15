33c21261634e269c0efb73b2659b3c008fcc5708
pfornasi <pfornasi@uwo.ca>
2023-03-15 17:58:29 -0400

    prepared for initial test

--- frontend/src/GASelect.js
+++ frontend/src/GASelect.js
@@ -13,7 +13,20 @@ function gAForm() {
     const [EE, setEE] = useState('');
     const [EPM, setEPM] = useState('');
     const [LL, setLL] = useState('');
-    const [formInput, setFormInput] = useState('');
+    const [formInput, setFormInput] = useState({
+        "Knowledge Base": KB,
+        "Problem Analysis": PA,
+        "Investigation": I,
+        "Design": D,
+        "Use of Engineering Tools": ET,
+        "Individual and Team Work": ITW,
+        "Communication Skills": CS,
+        "Professionalism": PR,
+        "Impact of Engineering on Society and the Environment": IESE,
+        "Ethics and Equity": EE,
+        "Economics and Project Management": EPM,
+        "Life-Long Learning": LL
+    });
 
     const handleKBChange = (event) => {
         setKB(event.target.value);
@@ -63,17 +76,27 @@ function gAForm() {
         setLL(event.target.value);
     };
 
-    const handleFormInputChange = (event) => {
-        setFormInput(event.target.value);
+    const handleFormInputChange = () => {
+        setFormInput({
+            "Knowledge Base": KB,
+            "Problem Analysis": PA,
+            "Investigation": I,
+            "Design": D,
+            "Use of Engineering Tools": ET,
+            "Individual and Team Work": ITW,
+            "Communication Skills": CS,
+            "Professionalism": PR,
+            "Impact of Engineering on Society and the Environment": IESE,
+            "Ethics and Equity": EE,
+            "Economics and Project Management": EPM,
+            "Life-Long Learning": LL
+        });
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
         // Do something with the form data here
-
-        // Clear the form input
-        setFormInput('');
+        handleFormInputChange();
     };
 
     return (
@@ -84,6 +107,7 @@ function gAForm() {
             <h2 data-attribute={"General Learning Objectives (CEAB Graduate Attributes)"}></h2>
 
             <div>
+                <h4>Knowledge Base</h4>
                 <select value={KB} onChange={handleKBChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -91,6 +115,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Problem Analysis</h4>
                 <select value={PA} onChange={handlePAChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -98,6 +123,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Investigation</h4>
                 <select value={I} onChange={handleIChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -105,6 +131,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Design</h4>
                 <select value={D} onChange={handleDChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -112,6 +139,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Use of Engineering Tools</h4>
                 <select value={ET} onChange={handleETChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -119,6 +147,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Individual and Team Work</h4>
                 <select value={ITW} onChange={handleITWChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -126,6 +155,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Communication Skills</h4>
                 <select value={CS} onChange={handleCSChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -133,6 +163,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Professionalism</h4>
                 <select value={PR} onChange={handlePRChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -140,6 +171,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Impact on Society and the Environment</h4>
                 <select value={IESE} onChange={handleIESEChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -147,6 +179,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Ethics and Equity</h4>
                 <select value={EE} onChange={handleEEChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -154,6 +187,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Economics and Project Management</h4>
                 <select value={EPM} onChange={handleEPMChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -161,6 +195,7 @@ function gAForm() {
                     <option value="A">Advanced</option>
                 </select>
 
+                <h4>Life-Long Learning</h4>
                 <select value={LL} onChange={handleLLChange}>
                     <option value="">Select Learning Objective</option>
                     <option value="I">Introductory</option>
@@ -171,10 +206,6 @@ function gAForm() {
             </div>
 
             <form onSubmit={handleSubmit}>
-                <label>
-                    Enter some data:
-                    <input type="text" value={formInput} onChange={handleFormInputChange} />
-                </label>
                 <button type="submit">Submit</button>
             </form>
         </div>
