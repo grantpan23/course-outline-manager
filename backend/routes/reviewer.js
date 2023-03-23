898037ce33d8a340f274a2940b01095ba0cab105
jtan334 <jtan334@uwo.ca>
2023-03-23 19:11:08 -0400

    changed some things for comments

--- backend/routes/reviewer.js
+++ backend/routes/reviewer.js
@@ -1,6 +1,39 @@
 const express = require('express');
-const router = express.Router();
+
+const router = express.Router({mergeParams: true});
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const helpers = require('./helpers.js');
+const Schemas = require('../models/schemas.js');
+
+
+const EditHistory = Schemas.EditHistory;
+
+
 
 router.use(express.json());
 
 module.exports = router;
+
+router
+    .route('/approval')
+    .post  (async(req,res) => {
+            
+    const newChange = new EditHistory({
+        userID: req.body.userID,
+        timeStamp:req.body.timeStamp,
+        activity: req.body.activity,
+        docID: req.body.docID
+       
+    })
+
+    await newChange.save((err) => {
+        if(err){
+            return res.status(500).send(err);
+        } else {
+            return res.json(newChange);
+        }
+    })
+})
