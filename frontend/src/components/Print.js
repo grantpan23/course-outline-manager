84763580076e992988f3bf7345fcc37af094de1b
jtan334 <jtan334@uwo.ca>
2023-03-24 15:18:54 -0400

    got it to open into a new window

--- frontend/src/components/Print.js
+++ frontend/src/components/Print.js
@@ -14,19 +14,24 @@ function Print({}) {
   const  printPage = async() =>{
     const document = await fetch(process.env.REACT_APP_API_URL + `/api/documents/${q.documentID}`);
     const data = await document.json(); 
+    console.log(data)
+    let data2 = data.ops
+    console.log(data2)
     var cfg = {};
 
-var converter = new QuillDeltaToHtmlConverter(data, cfg);
+var converter = new QuillDeltaToHtmlConverter(data2, cfg);
 
 var html = converter.convert(); 
     let stringData =html
     console.log(stringData)
-  
-    // const quillContent = q;
-    // console.log(quillContent)
-    const newWindow = window.open();
+
+    var newWindow = window.open();
     console.log(stringData)
+
     newWindow.document.write(stringData);
+    window.onafterprint = event => {
+      console.log('after print');
+    };
     newWindow.print();
   }
     return (
