1cf4874362678cab68a4b740fdc595b7306ba997
ahuang95 <andy0207huang@gmail.com>
2023-03-24 02:32:14 -0400

    check jwt for EditHistory, instructorHistory, ReviewHome js files

--- frontend/src/pages/EditHistory.js
+++ frontend/src/pages/EditHistory.js
@@ -8,6 +8,12 @@ export default function InstructionHistory() {
   const [editHistory, seteditHistory] = useState([])
   const history = useNavigate()
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      history("/")
+    }
+  }, []);
 
 
 
